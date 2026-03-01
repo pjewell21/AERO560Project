6759f2330c55d15e0172a60e9fc95b93ea0df27c
Paige Jewell <paigejewell@Paiges-MacBook-Air-4.local>
2026-03-01 14:10:43 -0800

    SRP and Solar Position

---
+++ accelSRP.m
@@ -0,0 +1,16 @@
+function [aSRP] = accelSRP (area,Psr,JDvect,cr,mass)
+
+% Inputs:
+% area = area of spacecraft exposed to sun [m2]
+% Psr = solar radiation pressure
+% mass = mass of the spacecraft
+% JDvect = vector of Julian Dates
+
+% Outputs:
+% aSRP = vector of SRP values
+
+for i = 1:length(JDvect)
+    [lamda eps r_S] = solar_position(JDvect(i));
+    accelSRP(i) = 
+
+end
