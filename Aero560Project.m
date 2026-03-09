bd0e666edf1cf91c9767dcc021459a2441af4c9f
Paige Jewell <paigejewell@Paiges-MacBook-Air-4.local>
2026-03-08 17:03:17 -0700

    Updating 560 Project

--- Aero560Project.m
+++ Aero560Project.m
@@ -13,12 +13,48 @@ params.mS = 4; %[kg]
 params.IB = diag([0.542,0.392,0.258]); %[kgm^2]
 params.IS = diag([3,3,6]); %[kgm^2]
 
+%% Section 4.1
+
+% Set ThetaG Value
+thetaG_41 = deg2rad(12.3);
+
+% Initial States - Case 1
+alphaS0_41 = deg2rad(-120);
+betaS0_41 = deg2rad(20);
+gammaS0_41 = deg2rad(5);
+alphaS0dot_41 = deg2rad(0.01);
+betaS0dot_41 = deg2rad(0.01);
+gammaS0dot_41 = deg2rad(0.01);
+
+x0_41 = [alphaS0_41;betaS0_41;gammaS0_41;alphaS0dot_41;betaS0dot_41;gammaS0dot_41];
 
 %% Section 4.2
 
 % Set ThetaG Value
-thetaG = 
+thetaG_42 = deg2rad(12.3);
 
+% Initial States - Case 3
+alphaS0_42 = deg2rad(5);
+betaS0_42 = deg2rad(20);
+gammaS0_42 = deg2rad(5);
+alphaS0dot_42 = deg2rad(0.01);
+betaS0dot_42 = deg2rad(0.01);
+gammaS0dot_42 = deg2rad(0.01);
+
+alphaTgt_42 = deg2rad(0);
+betaTgt_42 = deg2rad(15);
+gammaTgt_42 = deg2rad(0);
+alphaTgtdot_42 = deg2rad(0);
+betaTgtdot_42 = deg2rad(0);
+gammaTgtdot_42 = deg2rad(0);
+
+x0_42 = [alphaS0_42;betaS0_42;gammaS0_42;alphaS0dot_42;betaS0dot_42;gammaS0dot_42];
+
+xTarget_42 = [alphaTgt_42;betaTgt_42;gammaTgt_42;alphaTgtdot_42;betaTgtdot_42;gammaTgtdot_42];
+
+% Gains
+kP = 0.1;
+kD = 1;
 
 
 
